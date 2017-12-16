8836b5e76d5e93b397dcba53098072a5de599f27
Qing Huang <hq1992518@msn.cn>
2017-12-15 18:05:24 -0800

    Add more html

--- CMPE285-TEAM-Project/app/views.py
+++ CMPE285-TEAM-Project/app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, request
 from app import app
 from .forms import ethicalForm, growthForm, indexForm, qualityForm, valueForm
+from random import *
 
 import quandl
 import datetime
@@ -25,7 +26,14 @@ def index():
             stock1 = "AAPL 50%"
             stock2 = "ADBE 30%"
             stock3 = "NSRGY 20%"
-            return render_template('ethical.html', s1=a1, title=title1, s2=stock1, s3=stock2, s4=stock3)
+            num1 = randint(amount-100, amount+100)
+            num2 = randint(amount-100, amount+100)
+            num3 = randint(amount-100, amount+100)
+            num4 = randint(amount-100, amount+100)
+            num5 = randint(amount-100, amount+100)
+            profolio_list = [num1, num2, num3, num4, num4]
+            #print profolio_list
+            return render_template('ethical.html', s1=a1, title=title1, s2=stock1, s3=stock2, s4=stock3, portFolioList=profolio_list)
         elif growth_form.amount2.data is not None:
             amount = growth_form.amount2.data
             a1 = "The investment amount is: ${:,}".format(amount)
@@ -33,6 +41,13 @@ def index():
             stock1 = "TSLA 50%"
             stock2 = "INTC 25%"
             stock3 = "AMZN 25%"
+            num1 = randint(amount-100, amount+100)
+            num2 = randint(amount-100, amount+100)
+            num3 = randint(amount-100, amount+100)
+            num4 = randint(amount-100, amount+100)
+            num5 = randint(amount-100, amount+100)
+            profolio_list = [num1, num2, num3, num4, num4]
+            return render_template('growth.html', s1=a1, title=title1, s2=stock1, s3=stock2, s4=stock3, portFolioList=profolio_list)
         elif index_form.amount3.data is not None:
             amount = index_form.amount3.data
             a1 = "The investment amount is: ${:,}".format(amount)
@@ -40,6 +55,12 @@ def index():
             stock1 = "VTI 33%"
             stock2 = "IXUS 33%"
             stock3 = "ILTB 33%"
+            num1 = randint(amount-100, amount+100)
+            num2 = randint(amount-100, amount+100)
+            num3 = randint(amount-100, amount+100)
+            num4 = randint(amount-100, amount+100)
+            num5 = randint(amount-100, amount+100)
+            profolio_list = [num1, num2, num3, num4, num4]
         elif quality_form.amount4.data is not None:
             amount = quality_form.amount4.data
             a1 = "The investment amount is: ${:,}".format(amount)
@@ -47,6 +68,12 @@ def index():
             stock1 = "FB 50%"
             stock2 = "MSFT 20%"
             stock3 = "SNE 30%"
+            num1 = randint(amount-100, amount+100)
+            num2 = randint(amount-100, amount+100)
+            num3 = randint(amount-100, amount+100)
+            num4 = randint(amount-100, amount+100)
+            num5 = randint(amount-100, amount+100)
+            profolio_list = [num1, num2, num3, num4, num4]
         elif value_form.amount5.data is not None:
             amount = value_form.amount5.data
             a1 = "The investment amount is: ${:,}".format(amount)
@@ -54,6 +81,12 @@ def index():
             stock1 = "NFLX 80%"
             stock2 = "TWTR 15%"
             stock3 = "GOOGL 5%"
+            num1 = randint(amount-100, amount+100)
+            num2 = randint(amount-100, amount+100)
+            num3 = randint(amount-100, amount+100)
+            num4 = randint(amount-100, amount+100)
+            num5 = randint(amount-100, amount+100)
+            profolio_list = [num1, num2, num3, num4, num4]
         
 
 
